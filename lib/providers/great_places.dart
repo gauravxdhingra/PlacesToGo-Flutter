f61bae4cafba274e4784551585fabe8593686b01
Gaurav Dhingra <grvdhingra1999@gmail.com>
2020-04-09 11:13:11 +0530

    Local Storage Database Testing Success

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -19,10 +19,25 @@ class GreatPlaces with ChangeNotifier {
     );
     _items.add(newPlace);
     notifyListeners();
-    DBHelper.insert('places', {
+    DBHelper.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
       'image': newPlace.image.path,
     });
+  }
+
+  Future<void> fetchandsetplaces() async {
+    final DataList = await DBHelper.getData('user_places');
+    _items = DataList.map(
+      (item) => Place(
+        id: item['id'],
+        title: item['title'],
+        location: null,
+        image: File(
+          item['image'],
+        ),
+      ),
+    ).toList();
+    notifyListeners();
   }
 }
